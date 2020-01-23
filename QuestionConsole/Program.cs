3b9d7591999a38f0ff1b38105b8c3f6f693ee10b
bthuntervargr <bthuntervargr@hotmail.com>
2020-01-23 15:13:01 +0200

    Add project files.

---
+++ QuestionConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace QuestionConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello! " + name);
+            Console.ReadKey();
+
+            Console.WriteLine("Where do you live?");
+            string name1 = Console.ReadLine();
+            Console.WriteLine(name1 + "?" + " Where is that?");
+            Console.ReadKey();
+
+            Console.WriteLine("That Sounds Nice!");
+            string name2= Console.ReadLine();
+            Console.WriteLine(name2 + "." + " I would like to go there some day...");
+            Console.ReadKey();
+
+           
+            Console.WriteLine("Maybe I could visit one day.");
+            string name3 = Console.ReadLine();
+            
+            Console.ReadKey();
+        }
+    }
+}
+
